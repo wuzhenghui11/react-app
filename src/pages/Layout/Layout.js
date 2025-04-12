db89156261c78e2647104d43db2b0cbd9998f0d2
wzh <wuzhenghui11@163.com>
2025-04-12 10:23:47 +0800

    修改样式

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -1,4 +1,4 @@
-import { Button } from 'antd'
+import { Button, Menu } from 'antd'
 // import { createContext, useState, useRef, useContext, forwardRef, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
@@ -6,20 +6,54 @@ import classNames from 'classnames'
 
 import './Layout.css'
 
+const items = [
+  {
+    key: '/about',
+    label: 'about',
+  },
+  {
+    key: '/article',
+    label: 'article',
+  },
+  {
+    key: '/learnReduxToolkitReduxReact',
+    label: 'learnReduxToolkitReduxReact',
+  },
+  {
+    key: '/searchParamsParams/1001?aa=12',
+    label: 'searchParamsParams',
+  },
+  {
+    key: '/other',
+    label: 'other',
+  },
+]
+
 function Layout () {
   const navigate = useNavigate()
   const type = 1
 
+  function menuClick (item) {
+    console.log(item);
+    const path = item.key
+    navigate(path)
+  }
+
   return (
     <div className="home">
       <div className="nav">
-        <div className={classNames("nav-item", {active: type === 1})}><Button type="primary" onClick={() => navigate('/login')}>login</Button></div>
-        <div className={classNames("nav-item", {active: type === 1})}><Button type="primary" onClick={() => navigate('/')}>home</Button></div>
-        <div className={classNames("nav-item", {active: type === 1})}><Button type="primary" onClick={() => navigate('/about')}>about</Button></div>
-        <div className={classNames("nav-item", {active: type === 1})}><Button type="primary" onClick={() => navigate('/article')}>learn 组件间传递属性</Button></div>
-        <div className={classNames("nav-item", {active: type === 1})}><Button type="primary" onClick={() => navigate('/learnReduxToolkitReduxReact')}>ReduxToolkitReduxReact</Button></div>
-        <div className={classNames("nav-item", {active: type === 1})}><Button type="primary" onClick={() => navigate('/searchParamsParams/1001?aa=22')}>searchParamsParams</Button></div>
-        <div className={classNames("nav-item", {active: type === 1})}><Button type="primary" onClick={() => navigate('/other')}>other</Button></div>
+        <div className="top-bar">
+          <div className={classNames("nav-item", {active: type === 1})}><Button type="primary" onClick={() => navigate('/login')}>login</Button></div>
+          <div className={classNames("nav-item", {active: type === 1})}><Button type="primary" onClick={() => navigate('/')}>home</Button></div>
+        </div>
+        <Menu
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme="dark"
+          items={items}
+          onClick={menuClick}
+        />
       </div>
       <div className="content">
         <Outlet/>
